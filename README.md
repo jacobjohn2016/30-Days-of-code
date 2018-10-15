482080b4aab18936eaae95b4e931589d5a4bd257
Jacob <kapsjacob@gmail.com>
2018-10-15 18:24:56 +0530

    checksum and readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # 30 Days of code
+
+Attempt to crack C programming by solving one program per day.
+Aim: Commit at least once every day for the next 30 days.
+
+## Checksum
+### 10/15/18
+Source: [GeeksForGeeks](https://www.geeksforgeeks.org/error-detection-computer-networks/)
+* In checksum error detection scheme, the data is divided into k segments each of m bits.
+* In the sender’s end the segments are added using 1’s complement arithmetic to get the sum. The sum is complemented to get the checksum.
+* The checksum segment is sent along with the data segments.
+* At the receiver’s end, all received segments are added using 1’s complement arithmetic to get the sum. The sum is complemented.
+* If the result is zero, the received data is accepted; otherwise discarded.
+
---
+++ checksum.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <math.h>
+
+unsigned long long bin(unsigned long long x)
+{
+    unsigned long long binary = 0, i = 1, r;
+    while (x > 0)
+    {
+        r = x % 2;
+        x = x / 2;
+        binary = binary + (r * i);
+        i = i * 10;
+    }
+    return binary;
+}
+
+unsigned long long dec(unsigned long long x)
+{
+    unsigned long long decimal = 0, i = 1, r;
+    while (x > 0)
+    {
+        r = x % 10;
+        x = x / 10 ;
+        decimal = decimal + (r * i);
+        i = i * 2;
+    }
+    return decimal;
+}
+
+int main() {
+    unsigned long long cs = 0, i, b, r;
+    unsigned long long x[20], sum;
+
+    printf("\nEnter number of inputs: ");
+    scanf("%llu",&r);
+    
+    for(i = 0; i < r; i++)
+    {
+        printf("\nEnter the number in binary: ");
+        scanf("%llu",&x[i]);
+        sum += dec(x[i]);
+    }
+
+    printf("\nSum is %llu\n",bin(sum));
+
+    b = bin(sum);
+    i = 1;
+    while(b > 0)
+    {
+        r = b%10;
+        b = b/10;
+        if(r == 0)
+            cs = cs + i;
+        i = i * 10;
+    }
+    printf("\nChecksum is %llu",cs);
+}
